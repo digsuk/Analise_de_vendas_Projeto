88c3c5c1e189c30016632da7d32706b51d34ed6f
d_dig <d_dig@DESKTOP-BQUVVIP>
2018-05-09 10:37:57 -0300

    Mudança na classe Telas Assistente

--- Analise de Vendas/src/telas/TelasAssistentes.java
+++ Analise de Vendas/src/telas/TelasAssistentes.java
@@ -6,9 +6,9 @@
  *  ções e entre outros;
  *------------------------------------------------
  * Histórico de modificação
- * Data    Autor    Descrição
- *       |        |
- *----------------------------------------------*/
+ * Data             Autor                   Descrição
+ * 09/05/2018 |  Diogo Souza      | Adição do método confirmar exclusão;
+ *----------------------------------------------------------------------*/
 
 package telas;
 
@@ -17,25 +17,14 @@ import javax.swing.JOptionPane;
 public class TelasAssistentes {
 	
 	private static int confirm;
-	
-	public TelasAssistentes(int confirm) {
-		super();
-		TelasAssistentes.confirm = confirm;
-	}
-    
-	public int getConfirm() {
-		return confirm;
-	}
-
-
-	public void setConfirm(int confirm) {
-		TelasAssistentes.confirm = confirm;
-	}
-
 
 	public static void ConfirmarCadastro() {
 		
 		confirm = JOptionPane.showConfirmDialog(null, "Você realmente deseja cadastrar?", "Cadastrado", JOptionPane.YES_NO_OPTION);
+	}
+	
+	public static void ConfirmarExclusao() {
+		confirm = JOptionPane.showConfirmDialog(null, "Você realmente deseja excluir?", "Excluído", JOptionPane.YES_NO_OPTION);
 	}
 	
 
